8feed4444143f431d3e357e91169093d9860f479
Jayes <anotherjayes@gmail.com>
2022-04-21 12:15:43 +0800

    Added new template for detailed blog page and a base HTML

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_post, name='list_post'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
